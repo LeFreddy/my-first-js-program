54e4bb30b29710f44bcc04b59f5de5e8bcde4f83
Freddy Held <74872641+LeFreddy@users.noreply.github.com>
2021-03-03 19:52:06 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const age = prompt ("Wie alt sind sie?");
+
+let output = (age <= 18)
+   ?"Nein, sie dürfen nicht zur Wahl gehen"
+   :"Ja sie dürfen zur Wahl gehen";
+
+console.log ("Alter :",age);
+
+console.log ("Antwort :", output);
+
+alert (output);
